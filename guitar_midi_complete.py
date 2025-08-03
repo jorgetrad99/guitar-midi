fc00272c0ae607b653022494a39009d87ff025d5
Jorge Trad <jetradl@gmail.com>
2025-08-03 13:06:10 -0600

    fix: correct audio sample rate setting key in ALSA configuration

--- guitar_midi_complete.py
+++ guitar_midi_complete.py
@@ -168,7 +168,7 @@ class GuitarMIDIComplete:
             self.fs.setting('audio.alsa.device', self.audio_device or 'hw:0,0')
             self.fs.setting('synth.gain', 2.0)
             self.fs.setting('audio.periods', 2)
-            self.fs.setting('audio.sample-rate', 44100)
+            self.fs.setting('audio.sample_rate', 44100)
             self.fs.setting('synth.cpu-cores', 4)
             
             self.fs.start()
